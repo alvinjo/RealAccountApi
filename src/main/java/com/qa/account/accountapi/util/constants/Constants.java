a3fbb0d465d612b581bf392fb0da4801bc6ca440
alvinjo <alvin.joseph0@gmail.com>
2018-11-28 10:39:28 +0000

    ref

--- src/main/java/com/qa/account/accountapi/util/constants/Constants.java
+++ src/main/java/com/qa/account/accountapi/util/constants/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String URL_BASE = "/accounts";
     public static final String URL_GET_ACCOUNTS = "/getAccounts";
     public static final String URL_GET_ACCOUNT_BY_ID = "/getAccountById/{id}";
-    public static final String URL_CREATE_ACCOUNT = "/create/account";
+    public static final String URL_CREATE_ACCOUNT = "/createAccount";
     public static final String URL_DELETE_ACCOUNT = "/deleteAccount/{id}";
     public static final String URL_UPDATE_ACCOUNT = "/updateAccount/{id}";
 
